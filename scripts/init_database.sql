d7da2c0efa1bd86e056b3fb7dd45b69bc0f14ca6
Salman Chaudhry <131833175+salmanzchaudhry@users.noreply.github.com>
2026-05-25 21:23:14 -0500

    Create init_database.sql for Data Warehouse setup
    Added SQL script to initialize Data Warehouse database with schemas.

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,15 @@
+-- Create Data Warehouse DB
 
+CREATE DATABASE DataWarehouse;
+
+-- can highlight text below to run seperately, then alt x to run and connect to the new database.
+
+USE DataWarehouse;
+
+CREATE SCHEMA bronze;
+-- can be found in security > Schemas > bronze
+
+CREATE SCHEMA silver;
+GO -- seperates batches when working with multiple SQL statements, can't run the whole CREATE script at once without it, will get a syntax error. Tells it to run silver first then create gold.
+CREATE SCHEMA gold;
+GO
